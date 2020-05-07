7c232a6fa44af2b07c336f7c572a9e968cf65daa
Ray <rayhao6@hotmail.com>
2020-05-07 09:05:20 +0800

    first commit

--- main.go
+++ main.go
@@ -9,6 +9,8 @@ import (
 func main() {
 	//fmt.Println(beego.AppPath)
 
+
+	
 	if beego.BConfig.RunMode == "dev" {
 		beego.BConfig.WebConfig.DirectoryIndex = true
 		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
